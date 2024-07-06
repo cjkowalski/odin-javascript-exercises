7d4dd258c8d0699319515d7d00b8c3c92bb37cb7
“carterkowalski” <172563884+cjkowalski@users.noreply.github.com>
2024-07-06 18:04:07 -0400

    Attempted Odin Project JavaScript Exercises... need to supplement material and learn more before attempting again!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray, toBeRemoveArray) {
+  toBeRemovedArray.forEach(element => {
+    const index = givenArray.indexOf(toBeRemoved);
+    if (index > -1) {
+     givenArray.splice(index, 1);
+    }
+  });
+  return givenArray;
 };
 
 // Do not edit below this line
